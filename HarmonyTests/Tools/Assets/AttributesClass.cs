4de82d433f36f7dc051441975860de18fb010b5a
Andreas Pardeike <andreas@pardeike.net>
2017-01-04 21:39:23 +0100

    Added 100% test coverage for Attributes

---
+++ HarmonyTests/Tools/Assets/AttributesClass.cs
@@ -0,0 +1,27 @@
+﻿using Harmony;
+using System;
+
+namespace HarmonyTests.Assets
+{
+	[HarmonyPatch]
+	public class NoAttributesClass
+	{
+		[HarmonyPrepare]
+		public void Method1() { }
+	}
+
+	[HarmonyPatch(typeof(string))]
+	[HarmonyPatch("foobar")]
+	[HarmonyPatch(new Type[] { typeof(float), typeof(string) })]
+	public class AllAttributesClass
+	{
+		[HarmonyPrepare]
+		public void Method1() { }
+
+		[HarmonyPrefix]
+		public void Method2() { }
+
+		[HarmonyPostfix]
+		public void Method3() { }
+	}
+}
---
+++ HarmonyTests/Tools/TestAttributes.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Harmony;
+using HarmonyTests.Assets;
+using System.Linq;
+
+namespace HarmonyTests.Tools
+{
+	[TestClass]
+	public class Test_Attributes
+	{
+		[TestMethod]
+		public void TestAttributes()
+		{
+			var type = typeof(AllAttributesClass);
+			var attr = type.GetCustomAttributes(false);
+			var attributes = attr.Cast<HarmonyPatch>().ToList();
+			var info = HarmonyPatch.Merge(attributes);
+			Assert.IsNotNull(info);
+			Assert.AreEqual(typeof(string), info.type);
+			Assert.AreEqual("foobar", info.methodName);
+			Assert.IsNotNull(info.parameter);
+			Assert.AreEqual(2, info.parameter.Length);
+			Assert.AreEqual(typeof(float), info.parameter[0]);
+			Assert.AreEqual(typeof(string), info.parameter[1]);
+
+		}
+	}
+}
