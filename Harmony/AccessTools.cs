2dc5fc5e7ed6e1cabf45d795ef1142ae15ce4117
Andreas Pardeike <andreas@pardeike.net>
2017-01-04 20:18:51 +0100

    Moved AccessCache to its own file

--- Harmony/AccessTools.cs
+++ Harmony/AccessTools.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -69,82 +68,6 @@ namespace Harmony
 		public static Type[] Types(this ParameterInfo[] pinfo)
 		{
 			return pinfo.Select(pi => pi.ParameterType).ToArray();
-		}
-	}
-
-	public class AccessCache
-	{
-		internal Dictionary<Type, Dictionary<string, FieldInfo>> fields = new Dictionary<Type, Dictionary<string, FieldInfo>>();
-		internal Dictionary<Type, Dictionary<string, PropertyInfo>> properties = new Dictionary<Type, Dictionary<string, PropertyInfo>>();
-		internal Dictionary<Type, Dictionary<string, Dictionary<Type[], MethodInfo>>> methods = new Dictionary<Type, Dictionary<string, Dictionary<Type[], MethodInfo>>>();
-
-		internal FieldInfo GetFieldInfo(Type type, string name)
-		{
-			Dictionary<string, FieldInfo> fieldsByType = null;
-			fields.TryGetValue(type, out fieldsByType);
-			if (fieldsByType == null)
-			{
-				fieldsByType = new Dictionary<string, FieldInfo>();
-				fields.Add(type, fieldsByType);
-			}
-
-			FieldInfo field = null;
-			fieldsByType.TryGetValue(name, out field);
-			if (field == null)
-			{
-				field = AccessTools.Field(type, name);
-				fieldsByType.Add(name, field);
-			}
-			return field;
-		}
-
-		internal PropertyInfo GetPropertyInfo(Type type, string name)
-		{
-			Dictionary<string, PropertyInfo> propertiesByType = null;
-			properties.TryGetValue(type, out propertiesByType);
-			if (propertiesByType == null)
-			{
-				propertiesByType = new Dictionary<string, PropertyInfo>();
-				properties.Add(type, propertiesByType);
-			}
-
-			PropertyInfo property = null;
-			propertiesByType.TryGetValue(name, out property);
-			if (property == null)
-			{
-				property = AccessTools.Property(type, name);
-				propertiesByType.Add(name, property);
-			}
-			return property;
-		}
-
-		internal MethodInfo GetMethodInfo(Type type, string name, Type[] arguments)
-		{
-			Dictionary<string, Dictionary<Type[], MethodInfo>> methodsByName = null;
-			methods.TryGetValue(type, out methodsByName);
-			if (methodsByName == null)
-			{
-				methodsByName = new Dictionary<string, Dictionary<Type[], MethodInfo>>();
-				methods.Add(type, methodsByName);
-			}
-
-			Dictionary<Type[], MethodInfo> methodsByArguments = null;
-			methodsByName.TryGetValue(name, out methodsByArguments);
-			if (methodsByArguments == null)
-			{
-				methodsByArguments = new Dictionary<Type[], MethodInfo>();
-				methodsByName.Add(name, methodsByArguments);
-			}
-
-			MethodInfo method = null;
-			methodsByArguments.TryGetValue(arguments, out method);
-			if (method == null)
-			{
-				method = AccessTools.Method(type, name, arguments);
-				methodsByArguments.Add(arguments, method);
-			}
-
-			return method;
 		}
 	}
 }
