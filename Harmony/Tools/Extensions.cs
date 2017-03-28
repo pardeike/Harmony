560d8381cc09159acd92b087437ff133b6150c42
Manuel Schweigert <Manuel Schweigert>
2017-03-28 16:41:45 +0200

    Add ReplaceMatchingSequence extension method.

--- Harmony/Tools/Extensions.cs
+++ Harmony/Tools/Extensions.cs
@@ -36,13 +36,15 @@ namespace Harmony
 		}
 	}
 
-	public static class CollectionExtensions
+	public static class EnumerableExtensions
 	{
 		public static IEnumerable<T> Do<T>(this IEnumerable<T> sequence, Action<T> action)
 		{
 			if (sequence == null) return null;
-			var enumerator = sequence.GetEnumerator();
-			while (enumerator.MoveNext()) action(enumerator.Current);
+			foreach (var item in sequence)
+			{
+				action(item);
+			}
 			return sequence;
 		}
 
@@ -64,6 +66,75 @@ namespace Harmony
 		public static T[] AddToArray<T>(this T[] sequence, T item)
 		{
 			return Add(sequence, item).ToArray();
+		}
+		public static IEnumerable<T> ReplaceMatchingSequence<T>(this IEnumerable<T> source, IEnumerable<T> elements, Func<IEnumerable<T>, IEnumerable<T>> replacer)
+		{
+			return ReplaceMatchingSequence(source, elements.Select(x => (Func<T, bool>)(y => y.Equals(x))), replacer);
+		}
+
+		public static IEnumerable<T> ReplaceMatchingSequence<T>(this IEnumerable<T> source, IEnumerable<Func<T, bool>> predicates, Func<IEnumerable<T>, IEnumerable<T>> replacer)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+
+			if (predicates == null)
+				throw new ArgumentNullException(nameof(predicates));
+
+			var p = predicates.ToArray();
+
+			if (p.Length == 0)
+				throw new ArgumentException("Count of predicates is zero.");
+
+			if (replacer == null)
+				throw new ArgumentNullException(nameof(replacer));
+
+			return ReplaceMatchingSequenceInner(source, predicates, replacer);
+		}
+
+		private static IEnumerable<T> ReplaceMatchingSequenceInner<T>(this IEnumerable<T> source, IEnumerable<Func<T, bool>> predicates, Func<IEnumerable<T>, IEnumerable<T>> replacer)
+		{
+			var p = predicates.ToArray();
+			var matchingIndex = -1;
+
+			var buffer = new List<T>();
+
+			foreach (var e in source)
+			{
+				// matched next predicate
+				if (p[matchingIndex + 1](e))
+				{
+					matchingIndex++;
+
+					// add item to buffer
+					buffer.Add(e);
+
+					// all predicates matched
+					if (matchingIndex + 1 == p.Length)
+					{
+						matchingIndex = -1;
+						foreach (var r in replacer(buffer))
+						{
+							yield return r;
+						}
+						buffer.Clear();
+					}
+				}
+				else
+				{
+					// no match, so clear buffer if any and reset index
+					foreach (var i in buffer)
+						yield return i;
+					buffer.Clear();
+					matchingIndex = -1;
+
+					// then return current item
+					yield return e;
+				}
+			}
+
+			// finished going through source, send out the buffer if any
+			foreach (var l in buffer)
+				yield return l;
 		}
 	}
 }
