fc7f3fc1c1ad98c87cd51ddf8e9ca6b3da88938a
Jerome Haltom <wasabi@cogito.cx>
2018-07-10 00:24:02 -0500

    Change HarmonyTests to new package format. Remove redundant config stuff.

--- HarmonyTests/Properties/AssemblyInfo.cs
+++ HarmonyTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,4 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HarmonyTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HarmonyTest")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("dee74efc-29a8-4704-8536-7da38d3999f7")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
