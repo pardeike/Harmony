a4dc35cc265a5d67d74d4e146f02e687fb786819
Maik Macho <nuda1998@gmail.com>
2020-01-24 21:02:50 +0100

    Re-enable TestGenericStructReturnTypes, fix VS error in HarmonyTests

--- HarmonyTests/Patching/GenericsPatches.cs
+++ HarmonyTests/Patching/GenericsPatches.cs
@@ -99,7 +99,7 @@ namespace HarmonyLibTests
 	[TestFixture]
 	public class GenericsPatches
 	{
-		// [Test]
+		[Test]
 		public void TestGenericStructReturnTypes()
 		{
 			var originalClass = typeof(MyList<>).MakeGenericType(typeof(int));
