39bb5925552acfcfe8dfe012a4c922b04022084e
Maik Macho <nuda1998@gmail.com>
2020-01-26 00:46:29 +0100

    Disable signing of Harmony and HarmonyTests, remove keys

--- Harmony/Properties/AssemblyInfo.cs
+++ Harmony/Properties/AssemblyInfo.cs
@@ -2,10 +2,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("HarmonyTests, PublicKey=" +
-	"0024000004800000940000000602000000240000525341310004000001000100ed7f" +
-	"0e5329a6217e33563cb5ac0195aa8548a82ce4171957180732308ea601987806f397" +
-	"ccad8e80ec60beab48438dae8ccbfbb17d53ef1c3871fefd14f1cd38addccbfe728f" +
-	"886f188a32297020c489dc9fd306b9ce673a00329673a6dbdc9551e9a242059a297c" +
-	"ec63e38f0197b1f3951a37e74a9224f690683909bffcf7bc")]
+[assembly: InternalsVisibleTo("HarmonyTests")]
 [assembly: Guid("69aee16a-b6e7-4642-8081-3928b32455df")]
