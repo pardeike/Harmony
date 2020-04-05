66747df512fb43fcb2987c5157bad3c8c500b00c
Maik Macho <nuda1998@gmail.com>
2020-04-05 22:01:12 +0200

    Fix issue #275 - Update MMC and add missing InternalsVisibleTo

--- Harmony/Properties/AssemblyInfo.cs
+++ Harmony/Properties/AssemblyInfo.cs
@@ -3,4 +3,9 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("HarmonyTests")]
+// MonoMod.Common uses IgnoresAccessChecksTo on its end,
+// but older versions of the .NET runtime bundled with older versions of Windows
+// require Harmony to expose its internals instead.
+// This is only relevant for when MonoMod.Common gets merged into Harmony.
+[assembly: InternalsVisibleTo("MonoMod.Utils.Cil.ILGeneratorProxy")]
 [assembly: Guid("69aee16a-b6e7-4642-8081-3928b32455df")]
