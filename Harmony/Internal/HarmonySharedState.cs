d3aba67b74eed181588500807a1604356ac59b40
Andreas Pardeike <andreas@pardeike.net>
2021-09-11 11:51:35 +0200

    removes global mutex in favor to locking locally

--- Harmony/Internal/HarmonySharedState.cs
+++ Harmony/Internal/HarmonySharedState.cs
@@ -6,73 +6,50 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
 
 namespace HarmonyLib
 {
 	internal static class HarmonySharedState
 	{
 		const string name = "HarmonySharedState";
-		static readonly Mutex mutex = new Mutex(false, name);
-		static Dictionary<MethodBase, byte[]> state = null;
-		static Dictionary<MethodInfo, MethodBase> originals = null;
+
+		// state/originals are set to instances stored in the global dynamic types static fields with the same name
+		static readonly Dictionary<MethodBase, byte[]> state;
+		static readonly Dictionary<MethodInfo, MethodBase> originals;
 
 		internal const int internalVersion = 101;
 		internal static int actualVersion = -1;
 
-		private static FieldInfo methodAddress = typeof(StackFrame).GetField("methodAddress", BindingFlags.Instance | BindingFlags.NonPublic);
-
-		static T WithState<T>(Func<T> action)
+		static HarmonySharedState()
 		{
-			T result = default;
-			var acquired = false;
-			try
+			DetourHelper.Runtime.OnMethodCompiled += (MethodBase method, IntPtr codeStart, ulong codeLen) =>
 			{
-				_ = mutex.WaitOne();
-				acquired = true;
+				if (method == null) return;
+				var info = GetPatchInfo(method);
+				if (info == null) return;
+				PatchFunctions.UpdateRecompiledMethod(method, codeStart, info);
+			};
 
-				if (state is null)
-				{
-					DetourHelper.Runtime.OnMethodCompiled += OnCompileMethod;
+			var type = CreateSharedStateType();
 
-					var type = CreateSharedStateType();
+			var versionField = type.GetField("version");
+			if ((int)versionField.GetValue(null) == 0)
+				versionField.SetValue(null, internalVersion);
+			actualVersion = (int)versionField.GetValue(null);
 
-					var versionField = type.GetField("version");
-					if ((int)versionField.GetValue(null) == 0)
-						versionField.SetValue(null, internalVersion);
-					actualVersion = (int)versionField.GetValue(null);
+			var stateField = type.GetField("state");
+			if (stateField.GetValue(null) is null)
+				stateField.SetValue(null, new Dictionary<MethodBase, byte[]>());
 
-					var stateField = type.GetField("state");
-					if (stateField.GetValue(null) is null)
-						stateField.SetValue(null, new Dictionary<MethodBase, byte[]>());
+			var originalsField = type.GetField("originals");
+			if (originalsField != null && originalsField.GetValue(null) is null)
+				originalsField.SetValue(null, new Dictionary<MethodInfo, MethodBase>());
 
-					var originalsField = type.GetField("originals");
-					if (originalsField != null && originalsField.GetValue(null) is null)
-						originalsField.SetValue(null, new Dictionary<MethodInfo, MethodBase>());
+			state = (Dictionary<MethodBase, byte[]>)stateField.GetValue(null);
 
-					state = (Dictionary<MethodBase, byte[]>)stateField.GetValue(null);
-
-					originals = new Dictionary<MethodInfo, MethodBase>();
-					if (originalsField != null) // may not exist in older versions
-						originals = (Dictionary<MethodInfo, MethodBase>)originalsField.GetValue(null);
-				}
-
-				result = action();
-			}
-			finally
-			{
-				if (acquired)
-					mutex.ReleaseMutex();
-			}
-			return result;
-		}
-
-		static void OnCompileMethod(MethodBase method, IntPtr codeStart, ulong codeLen)
-		{
-			if (method == null) return;
-			var info = GetPatchInfo(method);
-			if (info == null) return;
-			PatchFunctions.UpdateRecompiledMethod(method, codeStart, info);
+			originals = new Dictionary<MethodInfo, MethodBase>();
+			if (originalsField != null) // may not exist in older versions
+				originals = (Dictionary<MethodInfo, MethodBase>)originalsField.GetValue(null);
 		}
 
 		static Type CreateSharedStateType()
@@ -105,35 +82,31 @@ namespace HarmonyLib
 
 		internal static PatchInfo GetPatchInfo(MethodBase method)
 		{
-			return WithState(() =>
-			{
-				var bytes = state.GetValueSafe(method);
-				if (bytes is null) return null;
-				return PatchInfoSerialization.Deserialize(bytes);
-			});
+			byte[] bytes;
+			lock (state) bytes = state.GetValueSafe(method);
+			if (bytes is null) return null;
+			return PatchInfoSerialization.Deserialize(bytes);
 		}
 
 		internal static IEnumerable<MethodBase> GetPatchedMethods()
 		{
-			return WithState(() => state.Keys.ToArray());
+			lock (state) return state.Keys.ToArray();
 		}
 
 		internal static void UpdatePatchInfo(MethodBase original, MethodInfo replacement, PatchInfo patchInfo)
 		{
 			var bytes = patchInfo.Serialize();
-			_ = WithState<object>(() =>
-			{
-				state[original] = bytes;
-				originals[replacement] = original;
-				return null;
-			});
+			lock (state) state[original] = bytes;
+			lock (originals) originals[replacement] = original;
 		}
 
 		internal static MethodBase GetOriginal(MethodInfo replacement)
 		{
-			return WithState(() => originals.GetValueSafe(replacement));
+			lock (originals) return originals.GetValueSafe(replacement);
 		}
 
+		static readonly bool hasMethodAddress = AccessTools.GetFieldNames(typeof(StackFrame)).Contains("methodAddress");
+		static readonly AccessTools.FieldRef<StackFrame, long> methodAddressRef = AccessTools.FieldRefAccess<StackFrame, long>("methodAddress");
 		internal static MethodBase FindReplacement(StackFrame frame)
 		{
 			var frameMethod = frame.GetMethod();
@@ -141,8 +114,8 @@ namespace HarmonyLib
 
 			if (frameMethod is null)
 			{
-				if (methodAddress == null) return null;
-				methodStart = (long)methodAddress.GetValue(frame);
+				if (hasMethodAddress == false) return null;
+				methodStart = methodAddressRef(frame);
 			}
 			else
 			{
@@ -155,7 +128,7 @@ namespace HarmonyLib
 			if (methodStart == 0)
 				return frameMethod;
 
-			return WithState(() => originals.Keys.FirstOrDefault(replacement => replacement.GetNativeStart().ToInt64() == methodStart));
+			lock (originals) return originals.Keys.FirstOrDefault(replacement => replacement.GetNativeStart().ToInt64() == methodStart);
 		}
 	}
 }
