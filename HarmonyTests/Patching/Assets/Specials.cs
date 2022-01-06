7af1b2d03a8c94e653cd03fd971b978becc4fee5
Andreas Pardeike <andreas@pardeike.net>
2022-01-06 22:24:25 +0100

    more test crashing tests

--- HarmonyTests/Patching/Assets/Specials.cs
+++ HarmonyTests/Patching/Assets/Specials.cs
@@ -206,9 +206,6 @@ namespace HarmonyLibTests.Assets
 
 	public class MarshalledTestClass : MarshalByRefObject
 	{
-		public delegate void TestEvent();
-		public event TestEvent OnTestEvent;
-
 		public void Run()
 		{
 			Console.WriteLine("MarshalledTestClass.Run called");
@@ -231,6 +228,73 @@ namespace HarmonyLibTests.Assets
 
 	[HarmonyPatch(typeof(MarshalledTestClass), nameof(MarshalledTestClass.Handler))]
 	public class MarshalledTestClass_Patch
+	{
+		static void Prefix()
+		{
+		}
+	}
+
+	public class MarshalledWithEventHandlerTest1Class : MarshalByRefObject
+	{
+		public delegate void TestEvent();
+		public event TestEvent OnTestEvent;
+
+		public void Run()
+		{
+			Console.WriteLine("MarshalledWithEventHandlerTest1Class.Run called");
+			OnTestEvent += Handler;
+			Console.WriteLine("MarshalledWithEventHandlerTest1Class.Run called");
+		}
+
+		public void Handler()
+		{
+			try
+			{
+				Console.WriteLine("MarshalledWithEventHandlerTest1Class.Handler called");
+			}
+			catch
+			{
+				Console.WriteLine("MarshalledWithEventHandlerTest1Class.Handler exception");
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(MarshalledWithEventHandlerTest1Class), nameof(MarshalledWithEventHandlerTest1Class.Handler))]
+	public class MarshalledWithEventHandlerTest1Class_Patch
+	{
+		static void Prefix()
+		{
+		}
+	}
+
+	public class MarshalledWithEventHandlerTest2Class : MarshalByRefObject
+	{
+		public delegate void TestEvent();
+		public event TestEvent OnTestEvent;
+
+		public void Run()
+		{
+			Console.WriteLine("MarshalledWithEventHandlerTest2Class.Run called");
+			OnTestEvent += Handler;
+			_ = OnTestEvent.Method;
+			Console.WriteLine("MarshalledWithEventHandlerTest2Class.Run called");
+		}
+
+		public void Handler()
+		{
+			try
+			{
+				Console.WriteLine("MarshalledWithEventHandlerTest2Class.Handler called");
+			}
+			catch
+			{
+				Console.WriteLine("MarshalledWithEventHandlerTest2Class.Handler exception");
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(MarshalledWithEventHandlerTest2Class), nameof(MarshalledWithEventHandlerTest2Class.Handler))]
+	public class MarshalledWithEventHandlerTest2Class_Patch
 	{
 		static void Prefix()
 		{
--- HarmonyTests/Patching/Specials.cs
+++ HarmonyTests/Patching/Specials.cs
@@ -278,5 +278,47 @@ namespace HarmonyLibTests.Patching
 			new MarshalledTestClass().Run();
 			Console.WriteLine($"### MarshalledTestClass AFTER");
 		}
+
+		[Test]
+		public void Test_MarshalledWithEventHandler1()
+		{
+			Console.WriteLine($"### MarshalledWithEventHandlerTest1 TEST");
+
+			var patchClass = typeof(MarshalledWithEventHandlerTest1Class_Patch);
+			Assert.NotNull(patchClass);
+
+			var instance = new Harmony("test");
+			Assert.NotNull(instance, "Harmony instance");
+			var patcher = instance.CreateClassProcessor(patchClass);
+			Assert.NotNull(patcher, "Patch processor");
+			var patched = patcher.Patch();
+			Assert.AreEqual(1, patched.Count);
+			Assert.NotNull(patched[0]);
+
+			Console.WriteLine($"### MarshalledWithEventHandlerTest1 BEFORE");
+			new MarshalledWithEventHandlerTest1Class().Run();
+			Console.WriteLine($"### MarshalledWithEventHandlerTest1 AFTER");
+		}
+
+		[Test]
+		public void Test_MarshalledWithEventHandler2()
+		{
+			Console.WriteLine($"### MarshalledWithEventHandlerTest2 TEST");
+
+			var patchClass = typeof(MarshalledWithEventHandlerTest2Class_Patch);
+			Assert.NotNull(patchClass);
+
+			var instance = new Harmony("test");
+			Assert.NotNull(instance, "Harmony instance");
+			var patcher = instance.CreateClassProcessor(patchClass);
+			Assert.NotNull(patcher, "Patch processor");
+			var patched = patcher.Patch();
+			Assert.AreEqual(1, patched.Count);
+			Assert.NotNull(patched[0]);
+
+			Console.WriteLine($"### MarshalledWithEventHandlerTest2 BEFORE");
+			new MarshalledWithEventHandlerTest2Class().Run();
+			Console.WriteLine($"### MarshalledWithEventHandlerTest2 AFTER");
+		}
 	}
 }
