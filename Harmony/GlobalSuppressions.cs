93160f8603b5332ac7e7cf090f420e566ff2ad0d
Andreas Pardeike <andreas@pardeike.net>
2022-04-24 10:23:21 +0200

    less supression

--- Harmony/GlobalSuppressions.cs
+++ Harmony/GlobalSuppressions.cs
@@ -1,13 +1,11 @@
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations.")]
 [assembly: SuppressMessage("Performance", "CA1813:Avoid unsealed attributes")]
 [assembly: SuppressMessage("Performance", "CA1810:Initialize reference type static fields inline")]
 [assembly: SuppressMessage("Performance", "CA1822:Mark members as static")]
 [assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types")]
 [assembly: SuppressMessage("Design", "CA1018:Mark attributes with AttributeUsageAttribute")]
 [assembly: SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible")]
-[assembly: SuppressMessage("Style", "IDE0057:Use range operator")]
 
 [assembly: SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~F:HarmonyLib.Sandbox.SomeStruct_Net.b1")]
 [assembly: SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~F:HarmonyLib.Sandbox.SomeStruct_Net.b2")]
